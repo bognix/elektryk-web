5a5c8f9c31830c9b23156601c2cc445f05c03228
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2019-02-09 16:22:38 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <nuxt-link
+          class="navbar-item"
+          to="/">LOGO</nuxt-link>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarMain"
+        >
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+        </a>
+      </div>
+      <div
+        id="navbarMain"
+        class="navbar-menu"
+      >
+        <div class="navbar-end">
+          <a class="navbar-item">
+            Oferta
+          </a>
+          <a class="navbar-item">
+            Wykonane Realizacje
+          </a>
+          <a class="navbar-item">
+            O Firmie
+          </a>
+          <a class="navbar-item">
+            Kontakt
+          </a>
+        </div>
+      </div>
+    </nav>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="scss">
+@import '~/assets/theme.scss';
+
+html {
+  font-family: $family-primary;
+  font-size: $font-size;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="hero is-large">
+    <div class="hero-body">
+      <h1 class="title is-1">Elektryk</h1>
+      <h2 class="subtitle is-5">Zakład oglólno-budowlany elektryczno-pomiarowy Bartosz Flieger</h2>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  background-image: url('http://www.weyuma.com/img/0962/239.jpg');
+  background-size: cover;
+}
+</style>
